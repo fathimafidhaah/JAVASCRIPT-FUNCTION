b2f8a9bf81c70314c3da1553ac3d5d49db69b4c2
FATHIMA FIDHA <spk610520@gmail.com>
2024-11-08 10:10:08 +0530

    JAVASCRIPT-FUNCTIONS

---
+++ primenumber/prime.js
@@ -0,0 +1,20 @@
+function prime(a) {
+    var result="its prime"
+    for(i=2;i<a;i++){
+        if(a%i==0){
+            result="its not prime"
+        }
+    }
+    console.log(result);
+    
+    }
+    prime(5)
+
+
+
+
+
+
+
+
+
